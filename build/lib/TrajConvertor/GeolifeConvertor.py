98104ddba4c7c0aa4506d582714c8875cd925974
saasghari <saasghari@gmail.com>
2025-07-01 00:35:29 +0330

    GeolifeConvertor() function: add to package and test

--- build/lib/TrajConvertor/GeolifeConvertor.py
+++ build/lib/TrajConvertor/GeolifeConvertor.py
@@ -1,3 +1,44 @@
-def testfunction():
-    print("Hello! The test is successful.")
-    print("test2")
+import os
+
+def PLTtoCSV(source,destination):
+    # convert plt file to csv file
+    sf = open(source,"r")
+    df = open(destination,"w")
+
+    # ignore 6 first lines
+    for i in range(6):
+        sf.readline()
+    
+    # convert
+    lns=sf.readlines()
+    df.write("latitude,longitude\n")
+    for ln in lns:
+        sln=ln.split(",")
+        df.write(sln[0]+","+sln[1]+"\n")
+    
+    sf.close()
+    df.close()
+
+
+
+def GeolifeConvert(src,des):
+
+    files=os.listdir(src)
+
+    # progress bar
+    cnt=0   # counter for progress bar
+    tn=len(files)  # total number for progress bar
+
+    for fname in files:
+        srcfn=src+"\\"+fname
+        desfn=des+"\\T_"+str(cnt)+".csv"
+        PLTtoCSV(srcfn,desfn)
+
+        # progress bar
+        p=int(cnt*100/tn)
+        print("progress...",end=" ")
+        print('\033[92m'+str(p)+"%"+'\033[0m', end="\r")
+        cnt=cnt+1
+    # progress bar
+    print("progress...",end=" ")
+    print('\033[92m'+"100%"+'\033[0m', end="\r")
--- src/TrajConvertor/GeolifeConvertor.py
+++ src/TrajConvertor/GeolifeConvertor.py
@@ -1,3 +1,5 @@
+import os
+
 def PLTtoCSV(source,destination):
     # convert plt file to csv file
     sf = open(source,"r")
@@ -16,3 +18,27 @@ def PLTtoCSV(source,destination):
     
     sf.close()
     df.close()
+
+
+
+def GeolifeConvert(src,des):
+
+    files=os.listdir(src)
+
+    # progress bar
+    cnt=0   # counter for progress bar
+    tn=len(files)  # total number for progress bar
+
+    for fname in files:
+        srcfn=src+"\\"+fname
+        desfn=des+"\\T_"+str(cnt)+".csv"
+        PLTtoCSV(srcfn,desfn)
+
+        # progress bar
+        p=int(cnt*100/tn)
+        print("progress...",end=" ")
+        print('\033[92m'+str(p)+"%"+'\033[0m', end="\r")
+        cnt=cnt+1
+    # progress bar
+    print("progress...",end=" ")
+    print('\033[92m'+"100%"+'\033[0m', end="\r")
